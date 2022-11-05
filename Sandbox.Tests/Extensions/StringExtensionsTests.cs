bd71030e7ae4f726924d72879e379a53353d1d7a
Tom Walker <tpwalke2@gmail.com>
2022-11-05 03:47:13 -0400

    Add more Luhn tests; implement Luhn algorithm

--- Sandbox.Tests/Extensions/StringExtensionsTests.cs
+++ Sandbox.Tests/Extensions/StringExtensionsTests.cs
@@ -8,11 +8,21 @@ public class StringExtensionsTests
 {
     [TestCase("4111111111111111", true, 1)]
     [TestCase("79927398710", false, 3)]
+    [TestCase("49927398716", true, 6)]
+    [TestCase("1234567812345678", false, 0)]
+    [TestCase("374652346956782346957823694857692364857368475368", true, 8)]
+    [TestCase("0", true, 0)]
+    [TestCase("59", true, 9)]
+    [TestCase("00", true, 0)]
     public void LuhnValidationTester(string input, bool expectedIsValid, int expectedCheckDigit)
     {
         var result = input.ValidateLuhn();
-        
-        Assert.That(result.IsValid, Is.EqualTo(expectedIsValid));
-        Assert.That(result.CheckDigit, Is.EqualTo(expectedCheckDigit));
+
+        Assert.Multiple(() =>
+            {
+                Assert.That(result.IsValid, Is.EqualTo(expectedIsValid));
+                Assert.That(result.CheckDigit, Is.EqualTo(expectedCheckDigit));
+            }
+        );
     }
 }
--- Sandbox/Extensions/StringExtensions.cs
+++ Sandbox/Extensions/StringExtensions.cs
@@ -4,7 +4,22 @@ public static class StringExtensions
 {
     public static LuhnValidationResult ValidateLuhn(this string input)
     {
-        return new LuhnValidationResult(true, 1);
+        var checkDigit = input.GetLuhnCheckDigit();
+
+        return new LuhnValidationResult(int.Parse(input[^1..]) == checkDigit, checkDigit);
+    }
+    
+    private static int GetLuhnCheckDigit(this string input) {
+        var sum = 0;
+        for (var i = 2; i <= input.Length; i++) {
+            var index        = input.Length - i;
+            var multiplier   = i % 2 == 0 ? 2 : 1;
+            var currentValue = (input[index] - 48) * multiplier;
+            sum += currentValue % 10;
+            if (currentValue >= 10) sum++;
+        }
+
+        return (10 - sum % 10) % 10;
     }
 }
 
